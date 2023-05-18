ebe074801a7bce0395aa8b2017ad51ebd3b669cf
Peter Rowlands <peter@pmrowla.com>
2023-05-18 15:13:43 +0900

    tests: use real azure when DVC_TEST_AZURE_PATH is set

--- dvc_azure/tests/fixtures.py
+++ dvc_azure/tests/fixtures.py
@@ -1,3 +1,5 @@
+import os
+
 # pylint: disable=redefined-outer-name
 import pytest
 
@@ -5,11 +7,19 @@ from .cloud import Azure
 
 
 @pytest.fixture
-def make_azure(tmp_azure_path, azurite):
+def make_azure(request):
     def _make_azure():
+        if os.environ.get("DVC_TEST_AZURE_PATH"):
+            path = Azure.get_storagepath()
+        else:
+            path = None
+        connection_string = os.environ.get("DVC_TEST_AZURE_CONNECTION_STRING")
+        if not (path and connection_string):
+            path = request.getfixturevalue("tmp_azure_path")
+            connection_string = request.getfixturevalue("azurite")
         return Azure(
-            str(tmp_azure_path).replace("az://", "azure://"),
-            connection_string=azurite,
+            str(path).replace("az://", "azure://"),
+            connection_string=connection_string,
         )
 
     return _make_azure

9e67bb4cc63b17313798cb4b7fd6c34fa7311822
Peter Rowlands <peter@pmrowla.com>
2023-06-21 14:32:03 +0900

    spec: wrap async and sync overridden adlfs methods

--- dvc_azure/spec.py
+++ dvc_azure/spec.py
@@ -8,6 +8,14 @@ class AzureBlobFileSystem(  # pylint: disable=abstract-method
         kwargs["overwrite"] = True
         return super().put_file(*args, **kwargs)
 
+    async def _put_file(self, *args, **kwargs) -> None:
+        kwargs["overwrite"] = True
+        return await super()._put_file(*args, **kwargs)
+
     def rm(self, *args, **kwargs) -> None:
         kwargs["expand_path"] = False
         return super().rm(*args, **kwargs)
+
+    async def _rm(self, *args, **kwargs) -> None:
+        kwargs["expand_path"] = False
+        return await super()._rm(*args, **kwargs)
